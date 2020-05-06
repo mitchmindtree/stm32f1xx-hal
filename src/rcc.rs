bb2e5f348348ca021c3a38e44b854b266baa6426
Torkel Danielsson <torkel@voysys.se>
2020-05-06 22:49:03 +0900

    enable peripheral clock

--- src/rcc.rs
+++ src/rcc.rs
@@ -556,6 +556,8 @@ impl CFGR {
                     .otgfspre()
                     .variant(usb_prescaler)
             });
+
+            rcc.apb2enr.modify(|_, w| w.afioen().set_bit());
         }
 
         let vco_input_2 = match hse_prediv2 {
